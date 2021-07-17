2c9bfee6dc8b52131f8226145f3c8fead2d4c92f
Larry Richards <lrichards@iotatx.com>
2021-07-17 14:50:04 +0900

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gammondatetime',
+    version='1.0',
+    packages=['gammon_datetime'],
+    url='https://github.com/gammoncap/gammondatetime',
+    license='MIT',
+    author='Larry Richards',
+    author_email='lrichards@gammoncap.com',
+    description='a time zone aware general purpose datetime object'
+)
