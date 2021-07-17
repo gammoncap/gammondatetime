f778ac7deebf13d2bcbf4bccec277d50a2eac6d6
pythonlarry <lrichards@iotatx.com>
2021-07-17 15:09:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'python-dateutil==2.8.2',
         'pytz==2021.1',
         'numpy==1.21.0'
-      ]
+      ],
     url='https://github.com/gammoncap/gammondatetime',
     license='MIT',
     author='Larry Richards',
